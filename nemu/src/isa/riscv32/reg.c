d21103ed8d943d20091711f75fb24654bd4913b0
Zhu Xiaojuan <awuwuwu_2023@qq.com>
2024-08-11 16:30:27 +0800

    扫描内存

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,6 +24,12 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+	int reg_num = ARRLEN(regs);
+	int i;
+
+	for(i = 0;i<reg_num;i++) {
+		printf("%2d : %-8s%#-15x%-15d\n",i,regs[i],cpu.gpr[i],cpu.gpr[i]);
+	}
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
