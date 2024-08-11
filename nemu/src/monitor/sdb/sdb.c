36f5f7ceea1071c68007a0dc0c522d52120cd8c8
tracer-ysyx <tracer@ysyx.org>
2024-08-11 16:14:23 +0800

    > compile NEMU ysyx_22040000 朱孝娟 Linux furiosa-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:23 up  6:49,  4 users,  load average: 0.04, 0.05, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -117,7 +117,7 @@ static int cmd_x(char *args) {
 
   int i,j;
   for(i = 0;i<len;){
-	printf("addr:%#010x ",addr);
+	printf(ANSI_FMT("addr:%#010x ",ANSI_BG_BLUE),addr);
   
 	for(j = 0;i<len&&j<4;i++,j++){
 		word_t data = vaddr_read(addr,1);
