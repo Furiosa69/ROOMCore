96b6bed81d153b1406f6663886c7d5ddc7919db3
Zhu Xiaojuan <awuwuwu_2023@qq.com>
2024-08-16 23:46:17 +0800

    完善监视点

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -17,6 +17,7 @@
 #include <cpu/decode.h>
 #include <cpu/difftest.h>
 #include <locale.h>
+#include "../include/config/watchpoint.h"
 
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
@@ -31,6 +32,7 @@ static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
 
 void device_update();
+void wp_check();
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
@@ -40,7 +42,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 //监视点触发暂停
 #ifdef CONFIG_WATCHPOINT
-  IFDEF(CONFIG_WATCHPOINT, wp_difftest());
+  IFDEF(CONFIG_WATCHPOINT, wp_check());
 #endif
 }
 
