edb41656d4ee1d519c0d631ca6e1a0efe8fef035
Zhu Xiaojuan <awuwuwu_2023@qq.com>
2025-01-12 19:38:01 +0800

    addi不能连续执行指令

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,31 +1,17 @@
 module top(
 	input clk,
 	input rst,
-//	output reg [11:0] imm,
-//	output reg [4:0] rs1,
-//	output reg [2:0]  fun3,
-//	output reg [4:0] rd,
-//	output reg [6:0] fun1,
-//	output [31:0] rs1_data,
-	output reg [31:0] pc,
-	output reg [31:0]	ins,
 	output [31:0] result
 );
 
-	reg [11:0] imm;
-	reg [4:0] rs1;
-	reg [2:0] fun3;
-	reg [4:0] rd;
-	reg [6:0] fun1;
-  //other
- reg [31:0] rs1_data;
 
-  initial begin
-  	pc = 32'h80000000;
-  end
-	
-  //触发器 
-	Reg #(32,32'b0) i0(clk,rst,pc,pc,1'b1);
+	wire [31:0] ins;
+	wire [31:0] pc;
+	wire [11:0] imm;
+	wire [4:0] rs1;
+	wire [2:0] fun3;
+	wire [4:0] rd;
+	wire [6:0] fun1;
 
 	ysyx_24080018_IFU ifu(
 		.clk(clk),
@@ -49,8 +35,11 @@ module top(
 	ysyx_24080018_EXU exu(
 		.clk(clk),
 		.rst(rst),
+		.pc(pc),
+		.rs1(rs1),
+		.rd(rd),
+		.fun1(fun1),
 		.fun3(fun3),
-		.rs1_data(rs1_data),
 		.imm(imm),
 		.result(result)
 	);
